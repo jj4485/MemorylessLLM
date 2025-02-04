7271d441c6312fb31c653a32eefe998cd4bab366
Jonathan <jj4485@princeton.edu>
2025-02-04 15:01:24 -0500

    changed temperature again

--- rlhf/data.py
+++ rlhf/data.py
@@ -52,7 +52,7 @@ def output_text(input_text, model, tokenizer):
             num_return_sequences=1,
             do_sample=True,
             top_k=50,
-            temperature=0.5
+            temperature=0.7
         )
 
     # Decode the generated tokens back to text
